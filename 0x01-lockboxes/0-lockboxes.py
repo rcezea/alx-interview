74ef0839ff73e86f32e9d5da132a2b4b57c70311
Richard <rcezea007@gmail.com>
2024-05-02 21:16:59 +0100

    proud of this one

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+""" 0. Lock boxes """
+
+
+def canUnlockAll(boxes):
+    """Cracking the lock box"""
+    if not type(boxes) is list:
+        return 'not a list'
+    length = len(boxes)
+    keys = {0}
+    visited = set()
+    while 1:
+        if not keys:
+            return False
+        index = keys.pop()
+        if index not in visited:
+            visited.add(index)
+            keys.update(boxes[index])
+
+            for k in keys.copy():
+                keys.update(dict.fromkeys(boxes[k]))
+                visited.add(k)
+                if len(visited) == length:
+                    return True
+                keys.remove(k)
+
+            if len(visited) == length:
+                return True
+        else:
+            continue
