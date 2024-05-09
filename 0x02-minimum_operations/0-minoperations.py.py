480f54eebb40c09271e825b1b7259e87ef8938bd
Richard <rcezea007@gmail.com>
2024-05-09 06:58:23 +0100

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python module to 0. Minimum Operations"""
+
+
+def minOperations(n: int):
+    """Return the minimum operation required to get n characters"""
+    if n == 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
