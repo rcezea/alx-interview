19dd0e6088a452437b6bba03b7764558d0a2e1f7
Richard <rcezea007@gmail.com>
2024-05-20 05:00:58 +0100

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -26,7 +26,7 @@ log_pattern = re.compile(
 try:
     for line in sys.stdin:
         try:
-            match = log_pattern.match(line)
+            match = log_pattern.fullmatch(line)
             if not match:
                 continue
             parts = line.split()
