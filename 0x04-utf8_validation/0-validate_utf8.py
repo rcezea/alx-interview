20ecd31cbcc5686fc0f96d4b0172cc5559355cc8
Richard <rcezea007@gmail.com>
2024-05-22 11:30:22 +0100

    main

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,7 @@
 def validUTF8(data):
     """ returns true if validation passes else false"""
     extra_bits = 0
-    if not data or not type(data) == list:
+    if not data or not isinstance(data, list):
         return False
     for i in data:
         if extra_bits == 0:
@@ -24,7 +24,3 @@ def validUTF8(data):
                 return False
             extra_bits -= 1
     return extra_bits == 0
-
-
-data = [65]
-print(validUTF8(data))
