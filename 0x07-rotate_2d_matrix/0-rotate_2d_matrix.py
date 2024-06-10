bc8b3b7806e9741a590f087a8d176e5721da4081
Richard <rcezea007@gmail.com>
2024-06-10 15:49:02 +0100

    rotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,9 @@ def rotate_2d_matrix(matrix: list) -> None:
     :param matrix: nxn matrix
     :return: None
     """
-    matrix = [[matrix[i][j] for i in range(len(matrix))]
-              for j in range(len(matrix[0]))]
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i,n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     for i in range(len(matrix)):
         matrix[i].reverse()
